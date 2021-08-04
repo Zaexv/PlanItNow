933fd1026d7fcca1edc7de05bb95d6cbd865c50b
Eduardo P <zaexv@hotmail.com>
2021-08-04 20:12:47 +0200

    [ADD] Kotlin Backend [ADD] Kotlin Integrated in Project

--- app/src/main/java/com/backend/test/Test.kt
+++ app/src/main/java/com/backend/test/Test.kt
@@ -1,20 +1,16 @@
-package com.tfm.backend
+package com.backend.test
 
+import AllPlansQuery
 import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.coroutines.await
 import com.apollographql.apollo.exception.ApolloException
-import com.query.AllPlansQuery
-import com.apollographql.apollo.coroutines.await as await
 
-class Main {
-
-
+class Test {
 
     companion object {
         @JvmStatic fun main(args: Array<String>) {
-
-
             connectionTest();
         }
 
@@ -27,13 +23,13 @@ class Main {
                 .build();
 
             val response = apolloClient.query(AllPlansQuery()).enqueue(object:
-            ApolloCall.Callback<AllPlansQuery.Data>(){
+                ApolloCall.Callback<AllPlansQuery.Data>(){
                 override fun onResponse(response: Response<AllPlansQuery.Data>) {
 
-                    for(plan in response.data?.allPlans!!){
+                    for(plan in response.data?.allPlans()!!){
                         println(plan.toString());
                     }
-                    println(response.data?.allPlans);
+                    println(response.data?.allPlans()!!);
                 }
                 override fun onFailure(e: ApolloException){
                     println("Failed!");
@@ -51,13 +47,12 @@ class Main {
                 apolloClient.query(AllPlansQuery()).await();
 
 
-            for(plan in res2.data?.allPlans!!){
+            for(plan in res2.data?.allPlans()!!){
                 println(plan.toString());
             }
         }
 
     }
-
 
 
 
