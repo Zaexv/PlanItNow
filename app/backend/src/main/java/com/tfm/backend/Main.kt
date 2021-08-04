0623b2d97747a3ddee3f1741264faf0fd9d20696
Eduardo P <zaexv@hotmail.com>
2021-08-04 15:18:51 +0200

    [ADD] Backend Module in Kotlin

--- app/backend/src/main/java/com/tfm/backend/Main.kt
+++ app/backend/src/main/java/com/tfm/backend/Main.kt
@@ -1,25 +1,61 @@
 package com.tfm.backend
 
+import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
 import com.query.AllPlansQuery
+import com.apollographql.apollo.coroutines.await as await
 
 class Main {
 
+
+
     companion object {
         @JvmStatic fun main(args: Array<String>) {
-            testeo();
+
+
+            connectionTest();
         }
 
-        private fun testeo(){
-            val apolloClient = ApolloClient.builder()
+        private fun connectionTest(){
+
+            var res = "";
+
+            val apolloClient: ApolloClient = ApolloClient.builder()
                 .serverUrl("http://localhost:8000/graphql")
                 .build();
 
-            val response = apolloClient.query(AllPlansQuery());
+            val response = apolloClient.query(AllPlansQuery()).enqueue(object:
+            ApolloCall.Callback<AllPlansQuery.Data>(){
+                override fun onResponse(response: Response<AllPlansQuery.Data>) {
+
+                    for(plan in response.data?.allPlans!!){
+                        println(plan.toString());
+                    }
+                    println(response.data?.allPlans);
+                }
+                override fun onFailure(e: ApolloException){
+                    println("Failed!");
+                }
+            }
+            );
 
 
-            println(response.toString());
         }
+
+
+        //Esto teóricamente se puede llamar sólo desde corutinas.
+        suspend fun doQuery(apolloClient: ApolloClient){
+            val res2 =
+                apolloClient.query(AllPlansQuery()).await();
+
+
+            for(plan in res2.data?.allPlans!!){
+                println(plan.toString());
+            }
+        }
+
     }
 
 
